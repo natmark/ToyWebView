90ec228593602ccd279ee08dd65dd727929a4900
natmark <natmark0918@gmail.com>
2021-08-11 02:44:43 +0900

    Add demo html/css to ContentView

--- ToyWebViewExample/ToyWebViewExample/ContentView.swift
+++ ToyWebViewExample/ToyWebViewExample/ContentView.swift
@@ -2,8 +2,33 @@ import SwiftUI
 import ToyWebView
 
 struct ContentView: View {
+    let html = """
+        <body>
+            <p>hello</p>
+            <p class="inline">world</p>
+            <p class="inline">:)</p>
+            <div class="none"><p>this should not be shown</p></div>
+            <style>
+                .none {
+                    display: none;
+                }
+                .inline {
+                    display: inline;
+                }
+            </style>
+        </body>
+        """
+    let css = """
+        script, style {
+            display: none;
+        }
+        p, div {
+            display: block;
+        }
+        """
+
     var body: some View {
-        ToyWebView()
+        ToyWebView(html: html, css: css)
     }
 }
 
