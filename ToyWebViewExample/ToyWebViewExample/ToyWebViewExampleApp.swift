837bda339728101ee7e65af829a42bc346ba6ced
natmark <natmark0918@gmail.com>
2021-08-11 00:14:17 +0900

    Add ToyWebViewExample

---
+++ ToyWebViewExample/ToyWebViewExample/ToyWebViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyWebViewExampleApp.swift
+//  ToyWebViewExample
+//
+//  Created by Atsuya Sato on 2021/08/11.
+//
+
+import SwiftUI
+
+@main
+struct ToyWebViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
